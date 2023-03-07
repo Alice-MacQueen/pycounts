815ee8d24e02b74437967859bf8b30234941e4c0
Alice MacQueen <amacqueen@adaptivebiotech.com>
2023-03-07 11:47:47 -0800

    build: add imports of major functions into pycounts_macqueen module

--- src/pycounts_macqueen/__init__.py
+++ src/pycounts_macqueen/__init__.py
@@ -1,3 +1,7 @@
 # read version from installed package
 from importlib.metadata import version
-__version__ = version("pycounts")
+__version__ = version("pycounts_macqueen")
+
+# populate package namespace
+from pycounts_macqueen.pycounts import count_words
+from pycounts_macqueen.plotting import plot_words
